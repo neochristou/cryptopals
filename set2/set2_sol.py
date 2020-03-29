7c1730423c6b86ab0fe6f14349b31401084c3a49
neochristou <neophytos.christou19@gmail.com>
2020-03-29 15:03:16 +0300

    Challenge 9

---
+++ set2/set2_sol.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+from set1.set1_sol import * 
+
+def pkcs7_pad(text_bytes, size):
+    padding_size = size - (len(text_bytes) % size)
+    if padding_size == 0:
+        return text_bytes
+    padding_bytes = (padding_size * chr(padding_size)).encode()
+    return text_bytes + padding_bytes
+
+def encrypt_aes_ecb(plaintext, key):
+    cipher = AES.new(key, AES.MODE_ECB)
+    return cipher.encrypt(plaintext)
+
+def decrypt_aes_cbc(ciphertext, key, iv):
+    plaintext = ""
+    prev_cipher_block = iv 
+    for i in range(0, len(ciphertext), 16):
+        cipher_block = ciphertext[i:i+16]
+        dec_block = decrypt_aes_ecb(cipher_block, key)
+        dec_block_bytes = bytearray(dec_block, 'utf-8')
+        prev_block_bytes = bytearray(prev_cipher_block, 'utf-8')
+        print(len(dec_block_bytes))
+        print(len(prev_cipher_block))
+        plain_block = xor_hex(dec_block_bytes.hex(), prev_block_bytes.hex())
+        prev_cipher_block = cipher_block
+        plaintext += plain_block
+    return plaintext
+
+if __name__ == '__main__':
+    # Challenge 10
+    # enc_file = open('set2/10.txt', 'r')
+    # enc_text = bytearray(enc_file.read(), 'utf-8')
+    # ciphertext = codecs.decode(enc_text, 'base64')
+    # key = b'YELLOW SUBMARINE'
+    # iv = '\x00' * 16
+    # plaintext = decrypt_aes_cbc(ciphertext, key, iv) 
+    # print(plaintext)
+
+    # Check ecb encryption
+    # dec_file = open('set2/lyrics.txt', 'r')
+    # plaintext = bytearray(dec_file.read(), 'utf-8')
+    # key = b'YELLOW SUBMARINE'
+    # ciphertext = encrypt_aes_ecb(plaintext[:len(plaintext)- len(plaintext) % 8], key) 
+    # ciphertext = codecs.encode(ciphertext, 'base64')
+    # print(ciphertext)
+
+    # Challenge 9 
+    # padded_bytes = pkcs7_pad(b'YELLOW SUBMARINE', 20)
+    # print(padded_bytes)
