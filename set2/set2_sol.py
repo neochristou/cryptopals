3de485dc92c2c754cdd401b0b2b9c957ea33af40
neochristou <neophytos.christou19@gmail.com>
2020-04-01 17:10:23 +0300

    Challenge 13. Also changed detect block size function to take as argument the encrypting function

--- set2/set2_sol.py
+++ set2/set2_sol.py
@@ -2,12 +2,13 @@
 from set1.set1_sol import * 
 import random
 import string
+from typing import Callable
 
 def pkcs7_pad(text_bytes: bytes, size: int) -> bytes:
     if not isinstance(text_bytes, bytes) or not isinstance(size, int):
         raise TypeError
     padding_size = size - (len(text_bytes) % size)
-    if padding_size == 0:
+    if padding_size % size == 0:
         return text_bytes
     padding_bytes = (padding_size * chr(padding_size)).encode()
     return text_bytes + padding_bytes
@@ -15,6 +16,7 @@ def pkcs7_pad(text_bytes: bytes, size: int) -> bytes:
 def encrypt_aes_ecb(plaintext: bytes, key: bytes) -> bytes:
     if not isinstance(plaintext, bytes) or not isinstance(key, bytes):
         raise TypeError
+    plaintext = pkcs7_pad(plaintext, 16)
     cipher = AES.new(key, AES.MODE_ECB)
     return cipher.encrypt(plaintext)
 
@@ -94,24 +96,24 @@ def ecb_oracle(plaintext: bytes, key: bytes) -> bytes:
     ciphertext = encrypt_aes_ecb(plaintext, key)
     return ciphertext
 
-def detect_block_size(key: bytes) -> int:
-    if not isinstance(key, bytes):
+def detect_block_size(key: bytes, encryption_function: Callable) -> int:
+    if not isinstance(key, bytes) or not isinstance(encryption_function, Callable):
         raise TypeError
     plaintext = b'A'
     first_hit = False
     first_hit_len = 0
-    ciphertext = ecb_oracle(plaintext, key)
+    ciphertext = encryption_function(plaintext, key)
     prev_cipher_len = len(ciphertext)
     while not first_hit:
         first_hit_len += 1
-        ciphertext = ecb_oracle(plaintext, key)
+        ciphertext = encryption_function(plaintext, key)
         cipher_len = len(ciphertext)
         if cipher_len > prev_cipher_len:
             first_hit = True
         prev_cipher_len = cipher_len
         plaintext += b'A'
     while True:
-        ciphertext = ecb_oracle(plaintext, key)
+        ciphertext = encryption_function(plaintext, key)
         cipher_len = len(ciphertext)
         if cipher_len > prev_cipher_len:
             return len(plaintext) - first_hit_len
@@ -119,16 +121,16 @@ def detect_block_size(key: bytes) -> int:
             prev_cipher_len = cipher_len
             plaintext += b'A'
 
-def ecb_decrypt_byte_at_a_time(key: bytes) -> bytes:
-    if not isinstance(key, bytes):
+def ecb_decrypt_byte_at_a_time(key: bytes, encryption_function: Callable) -> bytes:
+    if not isinstance(key, bytes) or not isinstance(encryption_function, Callable):
         raise TypeError
-    block_size = detect_block_size(key)
-    ciphertext = ecb_oracle(b'A' * 50, key)
+    block_size = detect_block_size(key, encryption_function)
+    ciphertext = encryption_function(b'A' * 50, key)
     ciphertext = codecs.encode(ciphertext, 'base64').decode('utf-8')
     mode = detect_mode(bytes(ciphertext, 'utf-8'))
     if mode != 'ECB':
         raise Exception('Can only decrypt ECB mode')
-    encrypted_secret = ecb_oracle(b'', key)
+    encrypted_secret = encryption_function(b'', key)
     encrypted_secret_len = len(encrypted_secret)
     total_blocks = encrypted_secret_len // block_size # Find out how many block the encrypted secret is by itself
     decrypted_text = b''
@@ -137,22 +139,87 @@ def ecb_decrypt_byte_at_a_time(key: bytes) -> bytes:
     for curr_block in range(total_blocks):
         decrypted_block = b''
         for i in range(block_size):
-            target = ecb_oracle(pre_appended_input, key)[target_position:target_position+ 16] # The block we want to match
+            target = encryption_function(pre_appended_input, key)[target_position:target_position+ 16] # The block we want to match
             for c in string.printable:
                 plaintext_pad = (target_position - (curr_block * 16)) * b'A' + b'A' * (block_size - 1 - i) + decrypted_text + decrypted_block + bytes(c, 'utf-8') # Same as target, except last character (brute force it)
-                output_block = ecb_oracle(plaintext_pad, key)[target_position:target_position+ 16]
+                output_block = encryption_function(plaintext_pad, key)[target_position:target_position+ 16]
                 if target == output_block: # Check if output matches target until we get the correct character
                     decrypted_block += bytes(c, 'utf-8')
                     break
             pre_appended_input = pre_appended_input[:-1]
         decrypted_text += decrypted_block
     return decrypted_text
+
+def encode_cookie(query: bytes) -> dict:
+    if not isinstance(query, bytes):
+        raise TypeError
+    query_str = query.decode('utf-8')
+    params = query_str.split('&')
+    data = {}
+    for param in params:
+        split = param.split('=')
+        data[split[0]] = split[1]
+    return data 
+
+def profile_for(email: bytes) -> bytes:
+    if not isinstance(email, bytes):
+        raise TypeError
+    badchars = ['&', '=']
+    email_str = email.decode('utf-8')
+    for c in badchars:
+        if c in email_str:
+            email_str = email_str[:email_str.index(c)]
+    email = bytes(email_str, 'utf-8')
+    return b'uid=10&role=user&email=' + email
+
+def encrypt_profile(profile: bytes, key: bytes) -> bytes:
+    if not isinstance(profile, bytes) or not isinstance (key, bytes):
+        raise TypeError
+    return encrypt_aes_ecb(profile, key)
+
+def decrypt_profile(enc_profile: bytes, key: bytes) -> dict:
+    if not isinstance(enc_profile, bytes) or not isinstance (key, bytes):
+        raise TypeError
+    decr = decrypt_aes_ecb(enc_profile, key).decode('utf-8')
+    pad = ord(decr[-1:])
+    if pad > 0 and pad < 16:
+        decr = decr[:-pad]
+    return encode_cookie(bytes(decr, 'utf-8'))
+
+def create_admin_account(key: bytes) -> bytes:
+    if not isinstance (key, bytes):
+        raise TypeError
+    block_size = detect_block_size(key, encrypt_profile)
+    target_bytes = b'&role=admin' # The bytes we want to attach on the text
+    email_suffix = b'@testemail.com'
+    email_prefix = b'a'
+    profile = profile_for(email_prefix + email_suffix)
+    email_prefix += b'a' * (2 * block_size - (len(profile) % block_size)) # Create an extra block where we control the whole plaintext block
+    profile = profile_for(email_prefix + email_suffix) # Create a fake profile to get the blocks of the ciphertext
+    encrypted_fake_profile = encrypt_profile(profile, key)[:-block_size] # Throw away the last block
+    attach = encrypt_aes_ecb(pkcs7_pad(email_suffix + target_bytes, block_size), key) # Create a new last block by encrypting the bytes we want to attach, along with the last part of the email
+    encrypted_fake_profile += attach # Attach the last modified encrypted block
+    return encrypted_fake_profile
     
 if __name__ == '__main__':
-    # Challenge 12
+    # Challenge 13
     key = generate_random_bytes(16)
-    decrypted = ecb_decrypt_byte_at_a_time(key)
-    print(decrypted.decode('utf-8'))
+    admin_acc = create_admin_account(key)
+    admin_acc_decrypted = decrypt_profile(admin_acc, key)
+    print(admin_acc_decrypted)
+
+    # Test profile functions
+    # profile = profile_for(b'test@test.com&role=admin')
+    # print(profile)
+    # enc_profile = encrypt_profile(profile, key)
+    # print(enc_profile)
+    # decr_profile = decrypt_profile(enc_profile, key)
+    # print(decr_profile)
+
+    # Challenge 12
+    # key = generate_random_bytes(16)
+    # decrypted = ecb_decrypt_byte_at_a_time(key, ecb_oracle)
+    # print(decrypted.decode('utf-8'))
 
     # Challenge 11
     # plaintext = b'A' * 50
