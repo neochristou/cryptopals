fd3b8179b8860e6b29af130c3a9d1eea5c56d9c6
neochristou <neophytos.christou19@gmail.com>
2020-04-08 15:47:31 +0300

    Challenge 18

--- set3/set3_sol.py
+++ set3/set3_sol.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 from set1.set1_sol import * 
 from set2.set2_sol import * 
-import random
-import string
+import random, string, struct
 from typing import Callable
 
 key = generate_random_bytes(16)
@@ -69,11 +68,56 @@ def attack_padding_oracle_cbc(ciphertext: bytes, iv: bytes) -> bytes:
         decrypted += decrypt_block_padding_oracle(first_block, second_block, iv)
     return decrypted
 
+def decrypt_aes_ctr(ciphertext: bytes, key: bytes, nonce: bytes):
+    if not isinstance(ciphertext, bytes) or not isinstance(key, bytes) or not isinstance(nonce,bytes):
+        raise TypeError
+    counter = 0
+    block_size = 16
+    decrypted = b''
+    blocks = len(ciphertext) // block_size
+    for i in range(blocks):
+        to_decrypt = ciphertext[i * block_size: (i+1) * block_size]
+        keystream_block = encrypt_aes_ecb(nonce + struct.pack('<Q', counter), key)
+        counter += 1
+        decrypted += xor_bytes(to_decrypt, keystream_block)
+    excess = len(ciphertext) - blocks * block_size
+    to_decrypt = ciphertext[-excess:]
+    keystream_block = encrypt_aes_ecb(nonce + struct.pack('<Q', counter), key)
+    decrypted += xor_bytes(to_decrypt, keystream_block[:excess])
+    return decrypted
+
+def encrypt_aes_ctr(plaintext: bytes, key: bytes, nonce: bytes):
+    if not isinstance(plaintext, bytes) or not isinstance(key, bytes) or not isinstance(nonce,bytes):
+        raise TypeError
+    counter = 0
+    block_size = 16
+    encrypted = b''
+    blocks = len(plaintext) // block_size
+    for i in range(blocks):
+        to_encrypt = plaintext[i * block_size: (i+1) * block_size]
+        keystream_block = encrypt_aes_ecb(nonce + struct.pack('<Q', counter), key)
+        counter += 1
+        encrypted += xor_bytes(to_encrypt, keystream_block)
+    excess = len(plaintext) - blocks * block_size
+    to_encrypt = plaintext[-excess:]
+    keystream_block = encrypt_aes_ecb(nonce + struct.pack('<Q', counter), key)
+    encrypted += xor_bytes(to_encrypt, keystream_block[:excess])
+    return encrypted
+
 if __name__ == '__main__':
+    # Challenge 18
+    key = b'YELLOW SUBMARINE'
+    encrypted = base64.b64decode('L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==')
+    decrypted = decrypt_aes_ctr(encrypted, key, b'\x00' * 8)
+    print(decrypted)
+    encrypted = encrypt_aes_ctr(decrypted, key, b'\x00' * 8)
+    encrypted = base64.b64encode(encrypted)
+    print(encrypted)
+
     # Challenge 17
-    encrypted, iv = encrypt_random_string_cbc()
-    decrypted = attack_padding_oracle_cbc(encrypted, iv)
-    print(decrypted)
-    decrypted = decrypt_aes_cbc(encrypted, key, iv)
-    print(decrypted)
+    # encrypted, iv = encrypt_random_string_cbc()
+    # decrypted = attack_padding_oracle_cbc(encrypted, iv)
+    # print(decrypted)
+    # decrypted = decrypt_aes_cbc(encrypted, key, iv)
+    # print(decrypted)
 
