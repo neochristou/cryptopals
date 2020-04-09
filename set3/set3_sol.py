373cdb57dbdc995dbc32709bbbc4251dcf4fc394
neochristou <neophytos.christou19@gmail.com>
2020-04-09 19:56:21 +0300

    Challenge 19 (needs improvement)

--- set3/set3_sol.py
+++ set3/set3_sol.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from set1.set1_sol import * 
 from set2.set2_sol import * 
-import random, string, struct
+import random, string, struct, operator
 from typing import Callable
 
 key = generate_random_bytes(16)
+
 strings_to_encrypt = [b'MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc='
 b'MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=',
 b'MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==',
@@ -15,6 +16,48 @@ b'MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==',
 b'MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=',
 b'MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=',
 b'MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93' ]
+
+plaintexts = [
+b'SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==',
+b'Q29taW5nIHdpdGggdml2aWQgZmFjZXM=',
+b'RnJvbSBjb3VudGVyIG9yIGRlc2sgYW1vbmcgZ3JleQ==',
+b'RWlnaHRlZW50aC1jZW50dXJ5IGhvdXNlcy4=',
+b'SSBoYXZlIHBhc3NlZCB3aXRoIGEgbm9kIG9mIHRoZSBoZWFk',
+b'T3IgcG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==',
+b'T3IgaGF2ZSBsaW5nZXJlZCBhd2hpbGUgYW5kIHNhaWQ=',
+b'UG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==',
+b'QW5kIHRob3VnaHQgYmVmb3JlIEkgaGFkIGRvbmU=',
+b'T2YgYSBtb2NraW5nIHRhbGUgb3IgYSBnaWJl',
+b'VG8gcGxlYXNlIGEgY29tcGFuaW9u',
+b'QXJvdW5kIHRoZSBmaXJlIGF0IHRoZSBjbHViLA==',
+b'QmVpbmcgY2VydGFpbiB0aGF0IHRoZXkgYW5kIEk=',
+b'QnV0IGxpdmVkIHdoZXJlIG1vdGxleSBpcyB3b3JuOg==',
+b'QWxsIGNoYW5nZWQsIGNoYW5nZWQgdXR0ZXJseTo=',
+b'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=',
+b'VGhhdCB3b21hbidzIGRheXMgd2VyZSBzcGVudA==',
+b'SW4gaWdub3JhbnQgZ29vZCB3aWxsLA==',
+b'SGVyIG5pZ2h0cyBpbiBhcmd1bWVudA==',
+b'VW50aWwgaGVyIHZvaWNlIGdyZXcgc2hyaWxsLg==',
+b'V2hhdCB2b2ljZSBtb3JlIHN3ZWV0IHRoYW4gaGVycw==',
+b'V2hlbiB5b3VuZyBhbmQgYmVhdXRpZnVsLA==',
+b'U2hlIHJvZGUgdG8gaGFycmllcnM/',
+b'VGhpcyBtYW4gaGFkIGtlcHQgYSBzY2hvb2w=',
+b'QW5kIHJvZGUgb3VyIHdpbmdlZCBob3JzZS4=',
+b'VGhpcyBvdGhlciBoaXMgaGVscGVyIGFuZCBmcmllbmQ=',
+b'V2FzIGNvbWluZyBpbnRvIGhpcyBmb3JjZTs=',
+b'SGUgbWlnaHQgaGF2ZSB3b24gZmFtZSBpbiB0aGUgZW5kLA==',
+b'U28gc2Vuc2l0aXZlIGhpcyBuYXR1cmUgc2VlbWVkLA==',
+b'U28gZGFyaW5nIGFuZCBzd2VldCBoaXMgdGhvdWdodC4=',
+b'VGhpcyBvdGhlciBtYW4gSSBoYWQgZHJlYW1lZA==',
+b'QSBkcnVua2VuLCB2YWluLWdsb3Jpb3VzIGxvdXQu',
+b'SGUgaGFkIGRvbmUgbW9zdCBiaXR0ZXIgd3Jvbmc=',
+b'VG8gc29tZSB3aG8gYXJlIG5lYXIgbXkgaGVhcnQs',
+b'WWV0IEkgbnVtYmVyIGhpbSBpbiB0aGUgc29uZzs=',
+b'SGUsIHRvbywgaGFzIHJlc2lnbmVkIGhpcyBwYXJ0',
+b'SW4gdGhlIGNhc3VhbCBjb21lZHk7',
+b'SGUsIHRvbywgaGFzIGJlZW4gY2hhbmdlZCBpbiBoaXMgdHVybiw=',
+b'VHJhbnNmb3JtZWQgdXR0ZXJseTo=',
+b'QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=' ]
 
 def encrypt_random_string_cbc() -> bytes:
     iv = generate_random_bytes(16)
@@ -99,20 +142,61 @@ def encrypt_aes_ctr(plaintext: bytes, key: bytes, nonce: bytes):
         counter += 1
         encrypted += xor_bytes(to_encrypt, keystream_block)
     excess = len(plaintext) - blocks * block_size
-    to_encrypt = plaintext[-excess:]
-    keystream_block = encrypt_aes_ecb(nonce + struct.pack('<Q', counter), key)
-    encrypted += xor_bytes(to_encrypt, keystream_block[:excess])
+    if (excess > 0):
+        to_encrypt = plaintext[-excess:]
+        keystream_block = encrypt_aes_ecb(nonce + struct.pack('<Q', counter), key)
+        encrypted += xor_bytes(to_encrypt, keystream_block[:excess])
     return encrypted
 
+def encrypt_same_nonce_ctr() -> list:
+    nonce = b'\x00' * 8
+    ciphertexts = []
+    for plaintext in plaintexts:
+        plaintext = base64.b64decode(plaintext)
+        ciphertext = encrypt_aes_ctr(plaintext, key, nonce)
+        ciphertexts.append(ciphertext)
+    return ciphertexts
+
+def decrypt_same_nonce_ctr(ciphertexts: list) -> list:
+    if not isinstance(ciphertexts, list):
+        raise TypeError
+    max_len = len(max(ciphertexts, key=len))
+    score_list = [0] * max_len
+    for i in range(max_len): 
+        scores = {}
+        for j in range(256):
+            scores[j] = 0
+        score_list[i] = scores
+    best = [0] * max_len
+    plaintexts = [b''] * len(ciphertexts)
+    for ciphertext in ciphertexts:
+        for pos, c in enumerate(ciphertext):
+            for i in range(256):
+                cand = chr(c ^ i)
+                if cand.upper() in letterFrequency: 
+                    score_list[pos][i] += letterFrequency.get(cand.upper())
+    for i, score in enumerate(score_list):
+        best[i] = max(score.items(), key=operator.itemgetter(1))[0]
+    for i, ct in enumerate(ciphertexts):
+        for pos, c in enumerate(ct):
+            plaintexts[i] += bytes(chr((c ^ best[pos])), 'utf-8')
+    return plaintexts
+
 if __name__ == '__main__':
+    # Challenge 19
+    ciphertexts = encrypt_same_nonce_ctr()
+    plaintexts = decrypt_same_nonce_ctr(ciphertexts)
+    for plaintext in plaintexts:
+        print(str(plaintext))
+
     # Challenge 18
-    key = b'YELLOW SUBMARINE'
-    encrypted = base64.b64decode('L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==')
-    decrypted = decrypt_aes_ctr(encrypted, key, b'\x00' * 8)
-    print(decrypted)
-    encrypted = encrypt_aes_ctr(decrypted, key, b'\x00' * 8)
-    encrypted = base64.b64encode(encrypted)
-    print(encrypted)
+    # key = b'YELLOW SUBMARINE'
+    # encrypted = base64.b64decode('L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==')
+    # decrypted = decrypt_aes_ctr(encrypted, key, b'\x00' * 8)
+    # print(decrypted)
+    # encrypted = encrypt_aes_ctr(decrypted, key, b'\x00' * 8)
+    # encrypted = base64.b64encode(encrypted)
+    # print(encrypted)
 
     # Challenge 17
     # encrypted, iv = encrypt_random_string_cbc()
